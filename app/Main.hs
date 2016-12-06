827c56c4b2452f7af30da22415331af5f89db425
Diego Vera <omar.vera@gmail.com>
2016-12-06 22:36:12 +0100

    Add functions to get remote and merge name

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,12 @@ main :: IO ()
 main = do
   branchName <- getBranchName
   if branchName /= ""
-  then
+  then do
     putStrLn branchName
+    remoteName <- getRemoteName branchName
+    putStrLn remoteName
+    mergeName <- getMergeName remoteName branchName
+    putStrLn mergeName
   else do
     shortRevision <- getShortRevisionOfHead
     putStrLn shortRevision
--- src/GitLib.hs
+++ src/GitLib.hs
@@ -1,12 +1,17 @@
 module GitLib
     ( getBranchName
       , getShortRevisionOfHead
+      , getRemoteName
+      , getMergeName
     ) where
 
 import System.Process
 import System.Exit
+import Text.Printf
 
-getBranchName :: IO String
+type BranchName = String
+
+getBranchName :: IO BranchName
 getBranchName = do
     ( exitCode, symbolicRef, _ ) <- readProcessWithExitCode "git" ["symbolic-ref", "HEAD"] []
     if exitCode == ExitSuccess
@@ -24,6 +29,32 @@ getShortRevisionOfHead = do
     shortRevision <- readProcess "git" ["rev-parse", "--short", "HEAD"] []
     let shortRevisionToReturn = removeEndOfLine shortRevision
     return shortRevisionToReturn
+
+type RemoteName = String
+
+getRemoteName :: BranchName -> IO RemoteName
+getRemoteName branchName = do
+    ( exitCode, remoteName, _ ) <- readProcessWithExitCode "git" ["config", printf "branch.%s.remote" branchName] []
+    if exitCode == ExitSuccess
+    then do
+      return $ removeEndOfLine remoteName
+    else
+      return "origin"
+
+type MergeName = String
+
+getMergeName :: RemoteName -> BranchName -> IO MergeName
+getMergeName remoteName branchName = do
+      if remoteName == "origin"
+      then 
+        return $ printf "refs/heads/%s" branchName
+      else do
+        ( exitCode, mergeName, _ ) <- readProcessWithExitCode "git" ["config", printf "branch.%s.merge" branchName] []
+        if exitCode == ExitSuccess
+        then do
+          return $ removeEndOfLine mergeName
+        else
+          return ""
 
 -- Helper functions
 removeEndOfLine :: String -> String
